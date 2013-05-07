4bf339085ad8d231c65c520c4531ada465c54168
Demostenes Garcia <me@demogar.com>
2013-05-07 16:20:13 -0500

    triangles p2 passed

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -1,20 +1,21 @@
 # Triangle Project Code.
 
-# Triangle analyzes the lengths of the sides of a triangle
-# (represented by a, b and c) and returns the type of triangle.
-#
-# It returns:
-#   :equilateral  if all sides are equal
-#   :isosceles    if exactly 2 sides are equal
-#   :scalene      if no sides are equal
-#
-# The tests for this method can be found in
-#   about_triangle_project.rb
-# and
-#   about_triangle_project_2.rb
-#
+# dgarcia
+# --
+# http://ruby-doc.org/core-2.0/Array.html#method-i-assoc
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  sides_array = [a, b, c]
+  unique_array = sides_array.uniq
+
+  # no zeros
+  raise TriangleError, "Zero?" if sides_array.include?(0)
+  # no negative
+  raise TriangleError, "Negative?" if sides_array.min < 0
+  # sum of two, less than the other
+  raise TriangleError, "Wrong sizes?" if (a+b <= c) || (a+c <= b) || (b+c <= a)
+
+  # Which triangle it is?
+  unique_array.size === 1 ? :equilateral : (unique_array.size === 2 ? :isosceles : :scalene)
 end
 
 # Error class used in part 2.  No need to change this code.
